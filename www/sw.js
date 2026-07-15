2b0a0cb44aea4fd46b8091500c4bf9caafa5a37d
Gabriel Vieira <gativrek.vieira@gmail.com>
2026-07-15 09:00:00 -0300

    Scaffold the Capacitor project (config, styles, MIT license)

---
+++ www/sw.js
@@ -0,0 +1,10 @@
+// Self-destructing service worker.
+// Clears all caches and unregisters itself so the app always loads fresh from APK assets.
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.map(k => caches.delete(k))))
+      .then(() => self.registration.unregister())
+  );
+});
